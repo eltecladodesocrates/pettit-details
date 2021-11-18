2337c46efe9fcd87244ea02581a9b9bf38b5427c
Luis Rodriguez <eltecladodesocrates@gmail.com>
2021-11-18 17:13:07 -0500

    nav and hero

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className='container'>
+            {/* <div className='container'>
                 <p>Pettit Details copyright &copy; {new Date().getFullYear()} all rights reserved</p>
-            </div>
+            </div> */}
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,17 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='container header-container'> 
-                <ul>
-                    <h1><NavLink to='/' exact={true}><h1>PettitDetails Logo</h1></NavLink></h1>
+            <NavLink to='/' exact={true}><div className='logo-dbg'></div></NavLink>
+                <ul className='nav-bar'>             
                     <li><NavLink to='/'>Home</NavLink></li>
                     <li><NavLink to='/about'>About Us</NavLink></li>
                     <li><NavLink to='/contact'>Contact Us</NavLink></li>
                     <li><NavLink to='/Gallery'>Gallery</NavLink></li>
                 </ul>
+            </div>
+            <div className='hero-img hero-container'>
+                <h2 className='hero-title'>Lorem Ipsum</h2>
+                <p className='hero-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Neque volutpat ac tincidunt vitae semper quis lectus</p>
             </div>
         </header>
     )
